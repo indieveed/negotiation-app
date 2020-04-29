dd048dfdcb6a048be7f0cf30328e81ecfc532711
Alexander Chaplinskiy <chaplinskiy@sports.ru>
2020-04-29 03:15:48 +0300

    Fix public path

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 
 // vue.config.js
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production' ? 'https://indieveed.github.io/negotiation-app/' : './',
     chainWebpack: config => {
         const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
         types.forEach(type => addStyleResource(config.module.rule('stylus').oneOf(type)))
