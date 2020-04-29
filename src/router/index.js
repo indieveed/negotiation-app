f9d53ceb1c3ce2cbdbff313db8660974d210a157
Alexander Chaplinskiy <chaplinskiy@sports.ru>
2020-04-29 03:42:24 +0300

    Fix router for gh-pages

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,7 @@ Vue.use(VueRouter)
 
 const router = new VueRouter({
   mode: 'history',
-  base: process.env.BASE_URL,
+  base: process.env.NODE_ENV === 'production' ? '/negotiation-app/' : '/',
   routes
 })
 
